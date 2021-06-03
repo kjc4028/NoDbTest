01d26eaf9d6165db8d3ee3a94d0ba8b446476837
jckim <whd4028@gmail.com>
2021-06-03 23:18:44 +0900

    java 1.7에서 TLSv1.2 url 호출하기

--- NoDbTest/src/main/java/com/jc/pj/HomeController.java
+++ NoDbTest/src/main/java/com/jc/pj/HomeController.java
@@ -1,23 +1,42 @@
 package com.jc.pj;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.support.ExecutorServiceAdapter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Handles requests for the application home page.
@@ -56,6 +75,154 @@ public class HomeController {
 		model.addAttribute("testTxt", str );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/ips", method = RequestMethod.GET)
+	public void testPost(Model model, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			
+			TrustManager[] trustCerts = new TrustManager[]{
+				    new X509TrustManager() {
+				        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				            return null;
+				        }
+				        public void checkClientTrusted(
+				            java.security.cert.X509Certificate[] certs, String authType) {
+				        }
+				        public void checkServerTrusted(
+				            java.security.cert.X509Certificate[] certs, String authType) {
+				        }
+				    }
+				};
+			SSLContext sc = SSLContext.getInstance("TLSv1.2");
+			sc.init(null, trustCerts, new java.security.SecureRandom());
+			
+			//URL url = new URL("http://javaking75.blog.me/rss");
+			URL url = new URL("https://www.kiep.go.kr/kiepForGov24OpenAPI.do?startDate=20191118&endDate=20191121");
+
+			HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+			conn.setSSLSocketFactory(sc.getSocketFactory());
+			
+			//HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		    conn.setRequestMethod("GET"); 
+		    conn.setConnectTimeout(3000); // 3초 
+		    conn.setDoInput(true);
+		    conn.setDoOutput(true);
+
+		       // 응답 내용(BODY) 구하기        
+	        try (InputStream in = conn.getInputStream();
+	                ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+	            
+	            byte[] buf = new byte[1024 * 8];
+	            int length = 0;
+	            while ((length = in.read(buf)) != -1) {
+	                out.write(buf, 0, length);
+	            }
+	            System.out.println(new String(out.toByteArray(), "UTF-8"));            
+	        }
+	        conn.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+	}
+
+	@RequestMapping(value = "/ips2", method = RequestMethod.GET)
+	public void testPost2(Model model, HttpServletRequest request, HttpServletResponse response) {
+			
+			try {
+		         
+				HashMap< String, String > pList = new HashMap<String, String>();  
+				pList.put("test", "tVal");
+				
+				URL url = new URL("http://localhost:8080/pj/ips3"); // URL 설정 
+
+		            HttpURLConnection http = (HttpURLConnection) url.openConnection(); // 접속 
+		            //-------------------------- 
+		            //   전송 모드 설정 - 기본적인 설정 
+		            //-------------------------- 
+		            http.setDefaultUseCaches(false);
+		            http.setDoInput(true); // 서버에서 읽기 모드 지정 
+		            http.setDoOutput(true); // 서버로 쓰기 모드 지정  
+		            http.setRequestMethod("POST"); // 전송 방식은 POST S
+
+
+
+		            //--------------------------
+		            // 헤더 세팅
+		            //--------------------------
+		            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다 
+		            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+		            //http.setRequestProperty("content-type", "text/html");
+
+
+		            //-------------------------- 
+		            //   서버로 값 전송 
+		            //-------------------------- 
+		            StringBuffer buffer = new StringBuffer();
+
+		            //HashMap으로 전달받은 파라미터가 null이 아닌경우 버퍼에 넣어준다
+		            if (pList != null) {
+
+		                Set<String> key = pList.keySet();
+
+		                for (Iterator<String> iterator = key.iterator(); iterator.hasNext();) {
+		                    String keyName = (String) iterator.next();
+		                    String valueName = pList.get(keyName);
+		                    buffer.append(keyName).append("=").append(valueName);
+		                }
+		            } 
+
+		            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+			
+			  PrintWriter writer = new PrintWriter(outStream); 
+			  writer.write(buffer.toString()); 
+			  writer.flush();
+			 
+		            
+		            System.out.println(http.getResponseCode());
+			} catch (Exception e) {
+				System.out.println("error!!!" + e);
+				e.printStackTrace(); 
+			}
+		
+		//return "redirect:https://nid.naver.com/nidlogin.login";
+	
+	}
+	
+	@RequestMapping(value = "/ips3", method = RequestMethod.POST)
+	public String testPost3(Model model, HttpServletRequest request, HttpServletResponse response) {
+			
+		System.out.println("ips3...");  
+		
+		return "postTest";
+	
+	} 
+
+	@RequestMapping(value = "/ips4", method = RequestMethod.GET)
+	public String testPost4(Model model, HttpServletRequest request, HttpServletResponse response) {
+		
+		 
+		return "postTest2";
+		
+	}
+	
+	@RequestMapping(value = "/ips5", method = RequestMethod.GET)
+	public String testPost5(Model model, HttpServletRequest request, HttpServletResponse response) {
+		return "postTest";
+	}
+
+	@RequestMapping(value = "/rest", method = RequestMethod.GET)
+	public void testRest(Model model, HttpServletRequest request, HttpServletResponse response) {
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("kwd", "test");
+
+	//Test용 로컬 주소
+        String url = "http://localhost:8080/pj/ips3";
+        ResponseEntity<String> res = new RestTemplate().postForEntity(url, parameters, String.class);
+        System.out.println(res.getBody());
+        System.out.println(res.getStatusCode());
 	}
 	
 	@Async     
